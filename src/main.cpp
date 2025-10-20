dac820d1bc76ea9ed4dbfb451059ea83204ebac8
Lakbir Ibrahim <librahim@e3r11p6.1337.ma>
2025-10-20 02:29:32 +0100

    updated again

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yosabir <yosabir@student.42.fr>            +#+  +:+       +#+        */
+/*   By: librahim <librahim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 17:12:48 by librahim          #+#    #+#             */
-/*   Updated: 2025/10/19 23:28:16 by yosabir          ###   ########.fr       */
+/*   Updated: 2025/10/20 02:28:51 by librahim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,22 +52,28 @@ int main(int ac, char *av[])
         std::cerr << "Usage: " << av[0] << " <port> <password>\n";
         return 1;
     }
+
     std::string port = av[1];
+    if (port.size() == 0)
+    {
+        std::cerr << "error : <port> cant be empty" << std::endl;
+        return 1;
+    }
     if (isNumeric(port) == false)
     {
         std::cerr << "error : <port> is not numeric" << std::endl;
         return 1;
     }
     std::string pw = av[2];
+    if (pw.size() == 0)
+    {
+        std::cerr << "error : <password> cant be empty" << std::endl;
+        return 1;
+    }
     long long port_num = atol(av[1]);
     if (port_num < 1024 || port_num > 49100)
     {
         std::cerr << "error : <port> is out of range" << std::endl;
-        return 1;
-    }
-    if (port.size() == 0)
-    {
-        std::cerr << "error : <port> cant be empty" << std::endl;
         return 1;
     }
     Server s(av[1], av[2]);
